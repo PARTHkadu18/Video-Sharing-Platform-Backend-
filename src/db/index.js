a0428925cc964b71c2262db95b1d0168c8b321c7
Parth <parthkadu82908@gmail.com>
2025-04-04 00:49:49 +0530

    all setup configured

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-console.log(process.env.MONGODB_URL);
+// console.log(process.env.MONGODB_URL);
 const connectDB = async()=>{
     try{
         
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -82,6 +82,6 @@ userSchema.methods.generateRefreshToken=function(){
         {
             expiresIn:process.env.REFRESH_TOKEN_EXPIRY
         }
-
+    )
 }
 export const User = mongoose.model("User",userSchema)
